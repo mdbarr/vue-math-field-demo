25ce202c3fe937ce4f883ce0243017db97069c33
Mark Barr <mark@hyperingenuity.com>
2019-11-18 11:07:00 -0500

    vue-math-field-demo: initial demo prototyping

---
+++ src/App.vue
@@ -0,0 +1,183 @@
+<template>
+<v-app>
+  <v-content>
+    <v-container fluid>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          >
+          <v-text-field
+            v-model="displayPrecision"
+            label="Display Precision (decimal places)"
+            ></v-text-field>
+          <v-text-field
+            v-model="precision"
+            label="Return Value Precision (decimal places)"
+            ></v-text-field>
+          <v-switch
+            v-model="numeric"
+            class="ma-1"
+            label="Numeric Return Value"
+            ></v-switch>
+          <v-text-field
+            v-model="label"
+            label="Label"
+            ></v-text-field>
+          <v-text-field
+            v-model="hint"
+            label="Hint"
+            ></v-text-field>
+          <v-text-field
+            v-model="placeholder"
+            label="Placeholder"
+            ></v-text-field>
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
+          >
+          <v-sheet
+            elevation="12"
+            class="pa-12"
+            >
+            <v-math-field
+              v-model="model"
+              :precision="Number(precision)"
+              :displayPrecision="Number(displayPrecision)"
+              :numeric="numeric"
+              :label="label"
+              :hint="hint"
+              :placeholder="placeholder"
+              :shaped="shaped"
+              :outlined="outlined"
+              :rounded="rounded"
+              :solo="solo"
+              :single-line="singleLine"
+              :filled="filled"
+              :clearable="clearable"
+              :persistent-hint="persistentHint"
+              :loading="loading"
+              :flat="flat"
+              :counter="counterEn ? counter : false"
+              :dense="dense"
+              ></v-math-field>
+            <div class="mt-12 text-center">
+              Return Value: <span style="background-color: #ddd; padding: 4px;">{{ model }}</span>
+            </div>
+        </v-sheet>
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
+          >
+          <v-switch
+            v-model="shaped"
+            class="ma-1"
+            label="Shaped (requires Filled, Outlined or Solo)"
+            :disabled="!outlined && !filled && !solo"
+            ></v-switch>
+          <v-switch
+            v-model="outlined"
+            class="ma-1"
+            label="Outlined"
+            ></v-switch>
+          <v-switch
+            v-model="rounded"
+            class="ma-1"
+            label="Rounded (requires Filled, Outlined or Solo)"
+            :disabled="!filled && !outlined && !solo"
+            ></v-switch>
+          <v-switch
+            v-model="solo"
+            class="ma-1"
+            label="Solo"
+            :disabled="filled"
+            ></v-switch>
+          <v-switch
+            v-model="singleLine"
+            class="ma-1"
+            label="Single-line"
+            ></v-switch>
+          <v-switch
+            v-model="filled"
+            class="ma-1"
+            label="Filled"
+            :disabled="outlined || solo"
+            ></v-switch>
+          <v-switch
+            v-model="clearable"
+            class="ma-1"
+            label="Clearable"
+            ></v-switch>
+          <v-switch
+            v-model="persistentHint"
+            class="ma-1"
+            label="Persistent Hint"
+            ></v-switch>
+          <v-switch
+            v-model="loading"
+            class="ma-1"
+            label="Loading"
+            ></v-switch>
+          <v-switch
+            v-model="flat"
+            class="ma-1"
+            label="Flat (requires Solo)"
+            :disabled="!solo"
+            ></v-switch>
+          <v-switch
+            v-model="dense"
+            class="ma-1"
+            label="Dense"
+            ></v-switch>
+          <v-row>
+            <v-switch
+              v-model="counterEn"
+              class="ma-0 mr-2 ml-1"
+              label="Counter"
+              ></v-switch>
+            <v-slider
+              v-model="counter"
+              :disabled="!counterEn"
+              ></v-slider>
+          </v-row>
+        </v-col>
+    </v-row>
+  </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import VMathField from './components/VMathField';
+
+export default {
+  name: 'App',
+  components: { VMathField },
+  data: () => {
+    return {
+      model: '(3 in + 5 cm + 2 voxels * PI / 2)',
+      label: 'Voxels',
+      hint: 'Enter voxels or expression',
+      placeholder: '',
+      displayPrecision: 2,
+      precision: 8,
+      numeric: true,
+      shaped: false,
+      outlined: false,
+      rounded: false,
+      solo: false,
+      singleLine: false,
+      filled: false,
+      clearable: false,
+      persistentHint: false,
+      loading: false,
+      flat: false,
+      counterEn: false,
+      counter: 0,
+      dense: false
+    };
+  }
+};
+</script>
---
+++ src/components/VMathField.vue
@@ -0,0 +1,141 @@
+<template>
+<v-text-field
+  v-bind="$attrs"
+  v-model="current"
+  @change="evaluate"
+  @focus="focus"
+  @blur="blur"
+  :error="error"
+  :error-messages="message"
+  />
+</template>
+
+<script>
+const math = require('mathjs');
+
+math.createUnit('voxel', {
+  definition: '7.5 cm', aliases: [ 'voxel', 'voxels' ]
+});
+
+export default {
+  name: 'v-math-field',
+  props: {
+    value: {
+      type: [ Number, String ],
+      default: 0
+    },
+    units: {
+      type: String,
+      default: 'voxels'
+    },
+    displayPrecision: {
+      type: Number,
+      default: 2
+    },
+    precision: {
+      type: Number,
+      default: 4
+    },
+    notation: {
+      type: String,
+      default: 'fixed'
+    },
+    numeric: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data: () => {
+    return {
+      current: '',
+      mode: 'display',
+      raw: '',
+      pretty: '',
+      error: false,
+      message: ''
+    };
+  },
+  created () {
+    this.mode = 'display';
+    this.evaluate(this.value.toString());
+    this.current = this.pretty;
+  },
+  methods: {
+    round (number) {
+      const factor = Math.pow(10, this.precision);
+      return Math.round(number * factor) / factor;
+    },
+    focus (...args) {
+      this.mode = 'edit';
+      this.current = this.raw;
+      console.log('focused');
+      this.$emit('focus', ...args);
+    },
+    blur (...args) {
+      if (this.error === false) {
+        this.mode = 'display';
+        this.current = this.pretty;
+        console.log('blurred');
+        this.$emit('blur', ...args);
+      }
+    },
+    evaluate (value) {
+      if (value.length) {
+        this.raw = value;
+        try {
+          console.log(`evaluating "${ value }"`);
+
+          value = math.evaluate(value);
+          if (typeof value === 'number') {
+            value = math.unit(value, this.units);
+          } else {
+            value = value.to(this.units);
+          }
+
+          console.log(typeof value, value);
+
+          this.pretty = math.format(value, {
+            notation: this.notation,
+            precision: this.displayPrecision
+          });
+
+          this.error = false;
+          this.message = '';
+
+          if (this.numeric) {
+            const number = this.round(value.toNumber(this.units));
+            this.$emit('input', number);
+          } else {
+            this.$emit('input', this.pretty);
+          }
+        } catch (error) {
+          console.log('errored', error);
+          this.error = true;
+          this.message = error.message;
+        }
+      }
+    },
+    update () {
+      if (this.raw) {
+        this.evaluate(this.raw);
+        if (this.mode === 'display') {
+          this.current = this.pretty;
+        } else {
+          this.current = this.raw;
+        }
+      }
+    }
+  },
+  watch: {
+    precision () {
+      this.update();
+    },
+    displayPrecision () {
+      this.update();
+    },
+    numeric () {
+      this.update();
+    }
+  }
+};
+</script>
